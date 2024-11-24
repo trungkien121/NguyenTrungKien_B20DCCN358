5f00ac804f194d7fc31a6e1cea0c32e9a5b50274
trungkien121 <89633669+trungkien121@users.noreply.github.com>
2024-11-25 06:41:00 +0700

    update BE

--- BE/hieuthuoc/src/main/java/com/example/hieuthuoc/repository/ThuocRepo.java
+++ BE/hieuthuoc/src/main/java/com/example/hieuthuoc/repository/ThuocRepo.java
@@ -18,24 +18,25 @@ public interface ThuocRepo extends JpaRepository<Thuoc, Integer> {
     Boolean existsByMaThuoc(String maThuoc);
     Boolean existsByTenThuoc(String tenThuoc);
     
-    //Search thuoc
+//    Search thuoc
     @Query("SELECT t FROM Thuoc t " +
-            "LEFT JOIN t.loaiThuoc lt " +
-            "LEFT JOIN t.nhaSanXuat nsx " +
-            "LEFT JOIN t.danhMucThuoc dmt " +
-            "WHERE (:keyWord IS NULL OR (LOWER(t.tenThuoc) LIKE LOWER(CONCAT('%', :keyWord, '%')) " +
-            "OR LOWER(t.maThuoc) LIKE LOWER(CONCAT('%', :keyWord, '%')))) " +
-            "AND (:loaiThuoc IS NULL OR LOWER(lt.tenLoaiThuoc) LIKE LOWER(CONCAT('%', :loaiThuoc, '%'))) " +
-            "AND (:nhaSanXuat IS NULL OR LOWER(nsx.tenNhaSanXuat) LIKE LOWER(CONCAT('%', :nhaSanXuat, '%'))) " +
-            "AND (:danhMucThuoc IS NULL OR LOWER(dmt.tenDanhMuc) LIKE LOWER(CONCAT('%', :danhMucThuoc, '%'))) " +
-            "AND (:doiTuongSd IS NULL OR LOWER(t.doiTuongSd) LIKE LOWER(CONCAT('%', :doiTuongSd, '%'))) " +
-            "AND (:maxGiaBan IS NULL OR t.giaBan <= :maxGiaBan)")
-     Page<Thuoc> search(
-            @Param("keyWord") String keyWord, // Tìm kiếm chung cho tên thuốc và mã thuốc
-            @Param("loaiThuoc") String loaiThuoc,
-            @Param("nhaSanXuat") String nhaSanXuat,
-            @Param("danhMucThuoc") String danhMucThuoc,
-            @Param("doiTuongSd") String doiTuongSd,
-            @Param("maxGiaBan") Double maxGiaBan,
-            Pageable pageable);
+    	       "LEFT JOIN t.loaiThuoc lt " +
+    	       "LEFT JOIN t.nhaSanXuat nsx " +
+    	       "LEFT JOIN t.danhMucThuoc dmt " +
+    	       "WHERE (:keyWord IS NULL OR (LOWER(t.tenThuoc) LIKE LOWER(CONCAT('%', :keyWord, '%')) " +
+    	       "OR LOWER(t.maThuoc) LIKE LOWER(CONCAT('%', :keyWord, '%')))) " +
+    	       "AND (:loaiThuoc IS NULL OR LOWER(lt.tenLoai) LIKE LOWER(CONCAT('%', :loaiThuoc, '%'))) " +
+    	       "AND (:nhaSanXuat IS NULL OR LOWER(nsx.tenNhaSanXuat) LIKE LOWER(CONCAT('%', :nhaSanXuat, '%'))) " +
+    	       "AND (:danhMucThuoc IS NULL OR LOWER(dmt.tenDanhMuc) LIKE LOWER(CONCAT('%', :danhMucThuoc, '%'))) " +
+    	       "AND (:doiTuongSd IS NULL OR LOWER(t.doiTuongSd) LIKE LOWER(CONCAT('%', :doiTuongSd, '%'))) " +
+    	       "AND (:maxGiaBan IS NULL OR t.giaBan <= :maxGiaBan)")
+    	Page<Thuoc> search(
+    	       @Param("keyWord") String keyWord, // Tìm kiếm chung cho tên thuốc và mã thuốc
+    	       @Param("loaiThuoc") String loaiThuoc,
+    	       @Param("nhaSanXuat") String nhaSanXuat,
+    	       @Param("danhMucThuoc") String danhMucThuoc,
+    	       @Param("doiTuongSd") String doiTuongSd,
+    	       @Param("maxGiaBan") Double maxGiaBan,
+    	       Pageable pageable);
+
 }
