212d8e4787b84aa38fbd141bf36127c57fc15d8d
quangptit538 <tmq21012002@gmail.com>
2024-12-11 22:20:58 +0700

    .

--- front/src/app/components/layouts/layouts.module.ts
+++ front/src/app/components/layouts/layouts.module.ts
@@ -2,13 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { SidebarLeftComponent } from "./sidebar-top/sidebar-top-admin.component";
+import { SidebarLeftComponent } from "./sidebar-top/admin/sidebar-top-admin.component";
 import { RouterModule } from "@angular/router";
 // Import ngx-translate và các module liên quan
 import { TranslateModule } from "@ngx-translate/core";
 import { HttpClientModule } from "@angular/common/http";
 import { ThongbaoHeaderComponent } from "./header/thongbao-header/thongbao-header.component";
-import { SidebarTopUserComponent } from "./sidebar-top/sidebar-top-user.component";
+import { SidebarTopUserComponent } from "./sidebar-top/user/sidebar-top-user.component";
 
 @NgModule({
   declarations: [
